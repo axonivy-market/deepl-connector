a4bd991493ef0e0c6698fd0aa7d43a805739d0fb
Tobias Gretler <tobias.gretler@axonivy.com>
2023-02-28 12:30:19 +0100

    XIVY-10781 deepl connector
    -translates documents
    -translates text

---
+++ deepl-connector-demo/src/com/axonivy/connector/deepl/FileLoaderUtil.java
@@ -0,0 +1,26 @@
+package com.axonivy.connector.deepl;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+
+import ch.ivyteam.ivy.IvyConstants;
+import ch.ivyteam.ivy.environment.Ivy;
+
+@SuppressWarnings("restriction")
+public class FileLoaderUtil {
+
+  public static ch.ivyteam.ivy.scripting.objects.File loadFromWebContent(String relativePath) throws IOException, CoreException {
+    IProject project = Ivy.request().getProcessModelVersion().getProject();
+    IFile file = project.getFolder(IvyConstants.DIRECTORY_WEB_CONTENT).getFile(relativePath);
+    var ivyFile = new ch.ivyteam.ivy.scripting.objects.File(file.getName(), true);
+    java.nio.file.Path path = ivyFile.getJavaFile().toPath();
+    Files.copy(file.getContents(), path, StandardCopyOption.REPLACE_EXISTING);
+    return ivyFile;
+  }
+
+}
