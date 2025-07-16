6c5dbfb329fd63ec884b116cc38765e0462939a6
Ivy Marketplace Team <hcmc-octopus@axonactive.com>
2025-07-16 20:06:16 +0700

    MARP-1983 adapt end2end testing (#79)
    * Switch to external DeepL docker instance for tests
    
    Replaces the internal DeepLServiceMock and related static mock files with integration of the official DeepL mock server via Docker and Testcontainers. Updates CI workflow to build and publish the mock server image, adjusts test setup and documentation to use the external mock, and adds necessary dependencies for Testcontainers.
    
    ---------
    
    Co-authored-by: Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>

--- README.md
+++ README.md
@@ -12,14 +12,16 @@ To maintain this project and simulate requests there's no necessity to have a De
 ### Mocking
 Setup the mock service for DeepL requests, which provides static results. This mock is enabled when running JUnit tests, but it can be enabled for the normal Designer as follows:
 
+- Start Deepl mock server (published and maintained by [DeeplCom](https://github.com/DeepLcom)) either by [using docker](https://github.com/DeepLcom/deepl-mock?tab=readme-ov-file#using-docker)
+or by running it [manually](https://github.com/DeepLcom/deepl-mock?tab=readme-ov-file#manually), depending on your development workflow.
 - Use your File Manager to navigate into the `configuration` directory of your Axon.ivy Designer.
-- Create a file called `app.yaml` and add the following contents with an editor:
-    
+- Create a file called `app.yaml`, add the following contents with an editor and replace `{deepl-test-instance-url}` with the url of your deepl instance (by default, it will be localhost:3000):
+
     ```yaml
     # yaml-language-server: $schema=https://json-schema.axonivy.com/app/12.0.0/app.json
     RestClients:
       deepl-connector:
-        Url: '{ivy.app.baseurl}/api/mock'
+        Url: {deepl-test-instance-url}
         Properties:
           AUTH.deepLKey: notMyKey
         Features:
--- deepl-connector-test/src_test/com/axonivy/connector/deepl/test/DeepLTest.java
+++ deepl-connector-test/src_test/com/axonivy/connector/deepl/test/DeepLTest.java
@@ -3,13 +3,22 @@ package com.axonivy.connector.deepl.test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-import com.axonivy.connector.deepl.mock.DeepLServiceMock;
 import com.deepl.api.v2.client.SourceLanguage;
 import com.deepl.api.v2.client.TargetLanguage;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
@@ -24,10 +33,41 @@ import ch.ivyteam.ivy.rest.client.security.CsrfHeaderFeature;
 import ch.ivyteam.ivy.scripting.objects.File;
 import deepl.translate.Options;
 
+@Testcontainers
 @IvyProcessTest(enableWebServer = true)
-public class DeepLTest{
+public class DeepLTest {
 
   private static final BpmProcess DEEPL = BpmProcess.path("deepl/translate");
+  private static final String DEFAULT_TEST_INSTANCE_URL = "http://localhost:3000/v2";
+  private static final String DEFAULT_TEST_INSTANCE_KEY = "octopus-test";
+  private static final String TEST_TEXT_IN_ENGLISH = "proton beam";
+  private static final String RESULT_TEXT_IN_GERMAN = "Protonenstrahl";
+  private static final String TEST_FILE_NAME = "test.txt";
+  private static final String FINISHED_SET_UP_LOG_REGEX = ".*mock-proxy-server listening on port 3001.*";
+
+  /**
+   * This Docker image is built using the Dockerfile from the
+   * <a href="https://github.com/DeepLcom/deepl-mock">DeepL mock server
+   * repository</a>, which is published and maintained by
+   * <a href="https://github.com/DeepLcom">DeepLcom</a>.
+   * <p>
+   * This image is intended for <b>testing purposes only</b> and <b>should not be
+   * used</b> in production.
+   * <p>
+   * To build this image yourself, refer to: <a href=
+   * "https://github.com/DeepLcom/deepl-mock?tab=readme-ov-file#using-docker">
+   * https://github.com/DeepLcom/deepl-mock?tab=readme-ov-file#using-docker</a>
+   */
+  private static final DockerImageName DEEPL_TEST_IMAGE = DockerImageName
+      .parse("octopusaxonivy/mock-deepl-server:latest");
+
+  @Container
+  @SuppressWarnings("resource")
+  private final GenericContainer<?> deeplContainer = new GenericContainer<>(DEEPL_TEST_IMAGE)
+      .withNetworkAliases("octopus_deepl").withExposedPorts(3000, 3001)
+      .withCreateContainerCmdModifier(cmd -> cmd
+          .withHostConfig(HostConfig.newHostConfig().withPortBindings(buildPortBinding(3000), buildPortBinding(3001))))
+      .waitingFor(Wait.forLogMessage(FINISHED_SET_UP_LOG_REGEX, 1)).withStartupTimeout(Duration.ofMinutes(1));
 
   private interface Start {
     BpmElement TRANSLATE = DEEPL.elementName("text(String,TargetLanguage)");
@@ -36,34 +76,38 @@ public class DeepLTest{
     BpmElement DOCUMENT_ADVANCED = DEEPL.elementName("document(File,Options)");
   }
 
+  private static PortBinding buildPortBinding(int port) {
+    return new PortBinding(Ports.Binding.bindPort(port), new ExposedPort(port));
+  }
+
   @BeforeEach
   void setup(AppFixture fixture, IApplication app) {
-    fixture.config("RestClients.deepl-connector.Url", DeepLServiceMock.URI);
-    fixture.var("deepl-connector.apiKey", "notMyKey:fx");
+    fixture.config("RestClients.deepl-connector.Url", DEFAULT_TEST_INSTANCE_URL);
+    fixture.var("deepl-connector.apiKey", DEFAULT_TEST_INSTANCE_KEY);
     RestClients clients = RestClients.of(app);
     RestClient deepL = clients.find("deepl-connector");
     var testClient = deepL.toBuilder()
       .feature(CsrfHeaderFeature.class.getName())
-      .property("AUTH.deepLKey", "notMyKey:fx")
+      .property("AUTH.deepLKey", DEFAULT_TEST_INSTANCE_KEY)
       .toRestClient();
     clients.set(testClient);
   }
 
   @Test
-  public void translate(BpmClient bpmClient){
+  public void translate(BpmClient bpmClient) {
     ExecutionResult result = bpmClient.start()
       .subProcess(Start.TRANSLATE)
-      .withParam("text", "Hello World")
+      .withParam("text", TEST_TEXT_IN_ENGLISH)
       .withParam("targetLanguage", TargetLanguage.DE)
       .execute();
     deepl.translate.Data data = result.data().last();
-    assertThat(data.getOutput()).isEqualTo("Hallo Welt");
+    assertThat(data.getOutput()).isEqualTo(RESULT_TEXT_IN_GERMAN);
   }
 
   @Test
-  public void document(BpmClient bpmClient) throws IOException{
-    File original = new File("juhu.pdf");
-    original.write("hoi pdf");
+  public void document(BpmClient bpmClient) throws IOException {
+    File original = new File(TEST_FILE_NAME);
+    original.write(TEST_TEXT_IN_ENGLISH);
     ExecutionResult result = bpmClient.start()
       .subProcess(Start.DOCUMENT)
       .withParam("file", original)
@@ -74,22 +118,22 @@ public class DeepLTest{
     assertThat(translated).isNotNull();
     assertThat(translated.getJavaFile()).exists();
   }
-  
+
   @Test
-  public void translateAdvanced(BpmClient bpmClient){
+  public void translateAdvanced(BpmClient bpmClient) {
     ExecutionResult result = bpmClient.start()
       .subProcess(Start.TRANSLATE_ADVANCED)
-      .withParam("text", "Hello World")
+      .withParam("text", TEST_TEXT_IN_ENGLISH)
       .withParam("options", getDefaultOptions())
       .execute();
     deepl.translate.Data data = result.data().last();
-    assertThat(data.getOutput()).isEqualTo("Hallo Welt");
+    assertThat(data.getOutput()).isEqualTo(RESULT_TEXT_IN_GERMAN);
   }
 
   @Test
-  public void documentAdvanced(BpmClient bpmClient) throws IOException{
-    File original = new File("juhu.pdf");
-    original.write("hoi pdf");
+  public void documentAdvanced(BpmClient bpmClient) throws IOException {
+    File original = new File(TEST_FILE_NAME);
+    original.write(TEST_TEXT_IN_ENGLISH);
     ExecutionResult result = bpmClient.start()
       .subProcess(Start.DOCUMENT_ADVANCED)
       .withParam("file", original)
@@ -100,13 +144,13 @@ public class DeepLTest{
     assertThat(translated).isNotNull();
     assertThat(translated.getJavaFile()).exists();
   }
-  
+
   private Options getDefaultOptions() {
-	  Options opt = new Options();
-	  opt.setSourceLang(SourceLanguage.EN);
-	  opt.setTargetLang(TargetLanguage.DE);
-	  opt.setTagHandling("HTML");
-	  return opt;
+    Options opt = new Options();
+    opt.setSourceLang(SourceLanguage.EN);
+    opt.setTargetLang(TargetLanguage.DE);
+    opt.setTagHandling("html");
+    return opt;
   }
 
 }
