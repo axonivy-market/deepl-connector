efc6f9d1af4547d5df280c10814d507d70625016
Louis <louis.mueller@axonivy.com>
2025-11-25 12:08:12 +0100

    indicate compile problem

--- deepl-connector-demo/src/com/axonivy/connector/deepl/FileLoaderUtil.java
+++ deepl-connector-demo/src/com/axonivy/connector/deepl/FileLoaderUtil.java
@@ -1,25 +1,18 @@
 package com.axonivy.connector.deepl;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 
-import ch.ivyteam.ivy.IvyConstants;
-import ch.ivyteam.ivy.project.IIvyProject;
-
-@SuppressWarnings("restriction")
 public class FileLoaderUtil {
 
   public static ch.ivyteam.ivy.scripting.objects.File loadFromWebContent(String relativePath) throws IOException, CoreException {
-    IProject project = IIvyProject.current().getProject();
-    IFile file = project.getFolder(IvyConstants.DIRECTORY_WEB_CONTENT).getFile(relativePath);
-    var ivyFile = new ch.ivyteam.ivy.scripting.objects.File(file.getName(), true);
-    java.nio.file.Path path = ivyFile.getJavaFile().toPath();
-    Files.copy(file.getContents(), path, StandardCopyOption.REPLACE_EXISTING);
+    // IProcessModelVersion.current().project().
+    // IProject project = IIvyProject.current().getProject();
+    // IFile file = project.getFolder(IvyConstants.DIRECTORY_WEB_CONTENT).getFile(relativePath);
+    var ivyFile = new ch.ivyteam.ivy.scripting.objects.File("to-be-fixed", true);
+    // java.nio.file.Path path = ivyFile.getJavaFile().toPath();
+    // Files.copy(file.getContents(), path, StandardCopyOption.REPLACE_EXISTING);
     return ivyFile;
   }
 
