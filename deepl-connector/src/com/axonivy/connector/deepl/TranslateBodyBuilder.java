36fe59dc0a4e1de60042c6474c89717164a27b3f
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-01-26 09:39:03 +0700

    refactor code

--- deepl-connector/src/com/axonivy/connector/deepl/TranslateBodyBuilder.java
+++ deepl-connector/src/com/axonivy/connector/deepl/TranslateBodyBuilder.java
@@ -1,7 +1,11 @@
 package com.axonivy.connector.deepl;
 
 import ch.ivyteam.ivy.scripting.objects.File;
+
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
@@ -12,53 +16,39 @@ import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 
 import deepl.translate.Options;
 
-public class TranslateBodyBuilder {
-  private TranslateBodyBuilder() {
-  }
-
-  private static final String SOURCE_LANG = "source_lang";
-  private static final String TARGET_LANG = "target_lang";
-  private static final String SPLIT_SENTENCES = "split_sentences";
-  private static final String PRESERVE_FORMATTING = "preserve_formatting";
-  private static final String FORMALITY = "formality";
-  private static final String GLOSSARY_ID = "glossary_id";
-  private static final String TAG_HANDLING = "tag_handling";
-  private static final String NON_SPLITTING_TAGS = "non_splitting_tags";
-  private static final String OUTLINE_DETECTION = "outline_detection";
-  private static final String SPLITTING_TAGS = "splitting_tags";
-  private static final String IGNORE_TAGS = "ignore_tags";
+public final class TranslateBodyBuilder {
   private static final String FILE = "file";
   private static final String FILE_NAME = "filename";
 
+  private TranslateBodyBuilder() {
+  }
+
+  private enum Field {
+    SOURCE_LANG("source_lang"), TARGET_LANG("target_lang"), SPLIT_SENTENCES("split_sentences"),
+    PRESERVE_FORMATTING("preserve_formatting"), FORMALITY("formality"), GLOSSARY_ID("glossary_id"),
+    TAG_HANDLING("tag_handling"), NON_SPLITTING_TAGS("non_splitting_tags"), OUTLINE_DETECTION("outline_detection"),
+    SPLITTING_TAGS("splitting_tags"), IGNORE_TAGS("ignore_tags");
+
+    private final String fieldName;
+
+    Field(String fieldName) {
+      this.fieldName = fieldName;
+    }
+
+    public String fieldName() {
+      return fieldName;
+    }
+  }
+
   public static MultivaluedMap<String, String> buildFormBodyByOptions(Options options) {
     MultivaluedMap<String, String> body = new MultivaluedHashMap<>();
-    putIfNotNull(body, SOURCE_LANG, options.getSourceLang());
-    putIfNotNull(body, TARGET_LANG, options.getTargetLang());
-    putIfNotNull(body, SPLIT_SENTENCES, options.getSplitSentences());
-    putIfNotNull(body, PRESERVE_FORMATTING, options.getPreserveFormatting());
-    putIfNotNull(body, FORMALITY, options.getFormality());
-    putIfNotNull(body, GLOSSARY_ID, options.getGlossaryId());
-    putIfNotNull(body, TAG_HANDLING, options.getTagHandling());
-    putIfNotNull(body, NON_SPLITTING_TAGS, options.getNonSplittingTags());
-    putIfNotNull(body, OUTLINE_DETECTION, options.getOutlineDetection());
-    putIfNotNull(body, SPLITTING_TAGS, options.getSplittingTags());
-    putIfNotNull(body, IGNORE_TAGS, options.getIgnoreTags());
+    forEachOption(options, (key, value) -> body.put(key, List.of(String.valueOf(value))));
     return body;
   }
 
   public static FormDataMultiPart buildMultiPartFormBodyByOptionsAndFile(Options options, File file) {
     FormDataMultiPart body = new FormDataMultiPart();
-    putIfNotNull(body, SOURCE_LANG, options.getSourceLang());
-    putIfNotNull(body, TARGET_LANG, options.getTargetLang());
-    putIfNotNull(body, SPLIT_SENTENCES, options.getSplitSentences());
-    putIfNotNull(body, PRESERVE_FORMATTING, options.getPreserveFormatting());
-    putIfNotNull(body, FORMALITY, options.getFormality());
-    putIfNotNull(body, GLOSSARY_ID, options.getGlossaryId());
-    putIfNotNull(body, TAG_HANDLING, options.getTagHandling());
-    putIfNotNull(body, NON_SPLITTING_TAGS, options.getNonSplittingTags());
-    putIfNotNull(body, OUTLINE_DETECTION, options.getOutlineDetection());
-    putIfNotNull(body, SPLITTING_TAGS, options.getSplittingTags());
-    putIfNotNull(body, IGNORE_TAGS, options.getIgnoreTags());
+    forEachOption(options, body::field);
     if (file != null) {
       body.bodyPart(new FileDataBodyPart(FILE, file.getJavaFile()));
       if (StringUtils.isNotBlank(file.getName())) {
@@ -68,17 +58,30 @@ public class TranslateBodyBuilder {
     return body;
   }
 
-  private static void putIfNotNull(MultivaluedMap<String, String> map, String key, Object value) {
-    if (map == null || key == null || value == null) {
+  private static void forEachOption(Options options, BiConsumer<String, String> consumer) {
+    if (options == null || consumer == null) {
       return;
     }
-    map.put(key, List.of(String.valueOf(value)));
+    extractOptions(options).forEach((field, value) -> {
+      if (value != null) {
+        consumer.accept(field.fieldName(), String.valueOf(value));
+      }
+    });
   }
 
-  private static void putIfNotNull(FormDataMultiPart multipart, String name, Object value) {
-    if (multipart == null || name == null || value == null) {
-      return;
-    }
-    multipart.field(name, String.valueOf(value));
+  private static Map<Field, Object> extractOptions(Options options) {
+    Map<Field, Object> map = new EnumMap<>(Field.class);
+    map.put(Field.SOURCE_LANG, options.getSourceLang());
+    map.put(Field.TARGET_LANG, options.getTargetLang());
+    map.put(Field.SPLIT_SENTENCES, options.getSplitSentences());
+    map.put(Field.PRESERVE_FORMATTING, options.getPreserveFormatting());
+    map.put(Field.FORMALITY, options.getFormality());
+    map.put(Field.GLOSSARY_ID, options.getGlossaryId());
+    map.put(Field.TAG_HANDLING, options.getTagHandling());
+    map.put(Field.NON_SPLITTING_TAGS, options.getNonSplittingTags());
+    map.put(Field.OUTLINE_DETECTION, options.getOutlineDetection());
+    map.put(Field.SPLITTING_TAGS, options.getSplittingTags());
+    map.put(Field.IGNORE_TAGS, options.getIgnoreTags());
+    return map;
   }
 }
