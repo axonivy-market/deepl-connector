f3a31b7cb62c69cb03418753574b1749f49119ae
Loc Nguyen <loc.nguyenthanh@axonactive.com>
2026-08-28 10:26:54 +0700

    chore: convert project

--- deepl-connector/src_generated/rest/deepl-connector/com/deepl/api/v2/client/InlineResponse2007Improvements.java
+++ deepl-connector/src_generated/rest/deepl-connector/com/deepl/api/v2/client/InlineResponse2007Improvements.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * InlineResponse2007Improvements
  */
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2025-12-25T10:19:09.709120500+07:00[Asia/Bangkok]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2025-12-25T10:19:09.709120500+07:00[Asia/Bangkok]")
 
 public class InlineResponse2007Improvements {
   @JsonProperty("detected_source_language")
--- deepl-connector/src_generated/rest/deepl-connector/com/deepl/api/v2/client/UsageBreakdown.java
+++ deepl-connector/src_generated/rest/deepl-connector/com/deepl/api/v2/client/UsageBreakdown.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 /**
  * Breakdown of character usage by category.
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2025-12-25T10:19:09.709120500+07:00[Asia/Bangkok]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2025-12-25T10:19:09.709120500+07:00[Asia/Bangkok]")
 
 public class UsageBreakdown {
   @JsonProperty("total_characters")
